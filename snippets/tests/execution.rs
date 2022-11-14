a60cbd6e1b90085fb1c44539f00f99994b971d91
Patricio Calderon <patricio.calderon@sansano.usm.cl>
2022-11-14 23:37:03 +0100

    feat: wrap instruction test macro

--- snippets/tests/execution.rs
+++ snippets/tests/execution.rs
@@ -4,37 +4,16 @@ mod utils;
 use solana_sdk::{signature::Signer, program_pack::Pack};
 use solana_program_test::tokio;
 
-#[tokio::test]
-async fn test_get_rent() {
-    test_no_accounts_instruction!(get_rent);
-}
+test_instruction!(no_accounts, get_rent);
 
-#[tokio::test]
-async fn test_get_clock() {
-    test_no_accounts_instruction!(get_clock);
-}
+test_instruction!(no_accounts, get_clock);
 
-#[tokio::test]
-async fn gn_test_get_accounts() {
-    test_single_readonly_account_instruction!(get_accounts);
-}
+test_instruction!(single_readonly_account, get_accounts);
 
-#[tokio::test]
-async fn gn_test_get_account() {
-    test_single_readonly_account_instruction!(get_account);
-}
+test_instruction!(single_readonly_account, get_account);
 
-#[tokio::test]
-async fn gn_test_pack_account() {
-    test_single_readonly_account_instruction!(pack_account);
-}
+test_instruction!(single_readonly_account, pack_account);
 
-#[tokio::test]
-async fn gn_test_unpack_account() {
-    test_single_readonly_account_instruction!(unpack_account);
-}
+test_instruction!(single_readonly_account, unpack_account);
 
-#[tokio::test]
-async fn gn_test_check_rent() {
-    test_single_readonly_account_instruction!(check_rent);
-}
+test_instruction!(single_readonly_account, check_rent);
--- snippets/tests/utils.rs
+++ snippets/tests/utils.rs
@@ -1,50 +1,62 @@
 #![cfg(feature = "test-sbf")]
 
 #[macro_export]
-macro_rules! test_no_accounts_instruction {
-	($instruction_name:ident ) => {
-				let pt = solana_program_test::ProgramTest::new("snippets", snippets::id(), solana_program_test::processor!(snippets::processor::Processor::process));
-				let (mut banks_client, payer, recent_blockhash) = pt.start().await;
-				let transaction = solana_sdk::transaction::Transaction::new_signed_with_payer(
-						&[snippets::instruction::$instruction_name(&snippets::id()).unwrap()],
-						Some(&payer.pubkey()),
-						&[&payer],
-						recent_blockhash,
-				);
-				banks_client.process_transaction(transaction).await.unwrap();
+macro_rules! test_instruction {
+    (no_accounts, $instruction_name:ident) => {
+        #[tokio::test]
+        async fn $instruction_name() {
+            let pt = solana_program_test::ProgramTest::new(
+                "snippets",
+                snippets::id(),
+                solana_program_test::processor!(snippets::processor::Processor::process),
+            );
+            let (mut banks_client, payer, recent_blockhash) = pt.start().await;
+            let transaction = solana_sdk::transaction::Transaction::new_signed_with_payer(
+                &[snippets::instruction::$instruction_name(&snippets::id()).unwrap()],
+                Some(&payer.pubkey()),
+                &[&payer],
+                recent_blockhash,
+            );
+            banks_client.process_transaction(transaction).await.unwrap();
+        }
     };
-}
+    (single_readonly_account, $instruction_name:ident) => {
+        #[tokio::test]
+        async fn $instruction_name() {
+            let pt = solana_program_test::ProgramTest::new(
+                "snippets",
+                snippets::id(),
+                solana_program_test::processor!(snippets::processor::Processor::process),
+            );
+            let (mut banks_client, payer, recent_blockhash) = pt.start().await;
 
-#[macro_export]
-macro_rules! test_single_readonly_account_instruction {
-	($instruction_name:ident ) => {
-				let pt = solana_program_test::ProgramTest::new("snippets", snippets::id(), solana_program_test::processor!(snippets::processor::Processor::process));
-				let (mut banks_client, payer, recent_blockhash) = pt.start().await;
+            let account = solana_sdk::signature::Keypair::new();
+            let rent = banks_client.get_rent().await.unwrap();
+            let account_rent = rent.minimum_balance(snippets::state::__Account__::LEN);
+            let transaction = solana_sdk::transaction::Transaction::new_signed_with_payer(
+                &[solana_program::system_instruction::create_account(
+                    &payer.pubkey(),
+                    &account.pubkey(),
+                    account_rent,
+                    snippets::state::__Account__::LEN as u64,
+                    &snippets::id(),
+                )],
+                Some(&payer.pubkey()),
+                &[&payer, &account],
+                recent_blockhash,
+            );
+            banks_client.process_transaction(transaction).await.unwrap();
 
-				let account = solana_sdk::signature::Keypair::new();
-				let rent = banks_client.get_rent().await.unwrap();
-				let account_rent = rent.minimum_balance(snippets::state::__Account__::LEN);
-				let transaction = solana_sdk::transaction::Transaction::new_signed_with_payer(
-						&[solana_program::system_instruction::create_account(
-								&payer.pubkey(),
-								&account.pubkey(),
-								account_rent,
-								snippets::state::__Account__::LEN as u64,
-								&snippets::id(),
-						)],
-						Some(&payer.pubkey()),
-						&[&payer, &account],
-						recent_blockhash,
-				);
-				banks_client.process_transaction(transaction).await.unwrap();
-
-
-				let transaction = solana_sdk::transaction::Transaction::new_signed_with_payer(
-						&[snippets::instruction::$instruction_name(&snippets::id(), &account.pubkey()).unwrap()],
-						Some(&payer.pubkey()),
-						&[&payer],
-						recent_blockhash,
-				);
-				banks_client.process_transaction(transaction).await.unwrap();
+            let transaction = solana_sdk::transaction::Transaction::new_signed_with_payer(
+                &[
+                    snippets::instruction::$instruction_name(&snippets::id(), &account.pubkey())
+                        .unwrap(),
+                ],
+                Some(&payer.pubkey()),
+                &[&payer],
+                recent_blockhash,
+            );
+            banks_client.process_transaction(transaction).await.unwrap();
+        }
     };
 }
